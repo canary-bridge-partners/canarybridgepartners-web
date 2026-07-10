bec8961d1cf3e4692aa4736f4e4bcc60a0bfd1b9
canary-bridge-partners <kukka80@hotmail.com>
2026-07-10 12:10:01 +0100

    Add form submission alert in app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    alert(
+        "Thank you for contacting Canary Bridge Partners. We will contact you shortly."
+    );
+
+});
